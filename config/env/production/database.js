8c8d6dad24d27ccc291e8ad91704ef2fb21aac88
Sean Tsang <seantsang83@gmail.com>
2023-06-01 11:33:44 +0800

    switch to ts

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,18 @@
-
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
-  connection: {  
+  connection: {
     client: 'postgres',
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: false
       },
     },
-      debug: false,
+    debug: false,
   },
 });
