49b215ec9a0ee8827da80d08e0c5df1cae8021fe
seantsang <seantsang83@gmail.com>
2023-06-04 07:53:06 +0900

    add past event

---
+++ src/api/past/controllers/past.ts
@@ -0,0 +1,7 @@
+/**
+ * past controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::past.past');
---
+++ src/api/past/services/past.ts
@@ -0,0 +1,7 @@
+/**
+ * past service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::past.past');
